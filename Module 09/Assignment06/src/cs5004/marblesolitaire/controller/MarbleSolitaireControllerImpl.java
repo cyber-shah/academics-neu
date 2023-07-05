c3bb81fe48e3d855f23549d93232472088f9ce2f
cyberShah <shah.pranc@northeastern.edu>
2023-07-05 10:50:40 +0530

    implementing controller

--- Module 09/Assignment06/src/cs5004/marblesolitaire/controller/MarbleSolitaireControllerImpl.java
+++ Module 09/Assignment06/src/cs5004/marblesolitaire/controller/MarbleSolitaireControllerImpl.java
@@ -10,15 +10,15 @@ public class MarbleSolitaireControllerImpl implements MarbleSolitaireController 
     private final MarbleSolitaireView view;
     private final Readable readableInput;
 
-    MarbleSolitaireControllerImpl(MarbleSolitaireModel model,
-                                  MarbleSolitaireView view, Readable readableInput) {
+    public MarbleSolitaireControllerImpl(MarbleSolitaireModel model,
+                                         MarbleSolitaireView view, Readable readableInput) {
         if (model == null || view == null || readableInput == null) {
             throw new IllegalArgumentException("Model, view, and readable cannot be null");
         }
         this.model = model;
         this.view = view;
-        this.readableInput = readableInput;}
-
+        this.readableInput = readableInput;
+    }
 
     /**
      * Play a new game of Marble Solitaire using the model.
@@ -29,12 +29,65 @@ public class MarbleSolitaireControllerImpl implements MarbleSolitaireController 
      */
     @Override
     public void playGame() throws IllegalStateException {
+        // create a scanner
         Scanner scanner = new Scanner(readableInput);
-        while (model.isGameOver() != true) {
-            view.renderBoard();
-            view.renderMessage(Integer.toString(model.getScore()));
-            scanner.p
+        // while scanner has next Line
+        while (scanner.hasNextLine()) {
+            try {
+                // 1. render the board
+                view.renderBoard();
+                // 2. render the score
+                view.renderMessage(Integer.toString(model.getScore()));
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                throw new IllegalStateException("Error rendering the board or score");
+            }
+            // get the next Line
+            String Line = scanner.nextLine();
+            // values in the Line
+            String[] values = Line.split(" ");
+            if (!isBadInput(values)) {
+                try {
+                    view.renderMessage("Invalid move. Play again. " + values + " is not a valid input");
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    throw new IllegalStateException("Error rendering the message, invalid move");
+                }
+                continue;
+            }
+            model.move(Integer.parseInt(values[0]), Integer.parseInt(values[1]),
+                    Integer.parseInt(values[2]), Integer.parseInt(values[3]));
         }
-
     }
+
+    /**
+     * Checks if the input is bad.
+     *
+     * @param input the input to check.
+     * @return true if the input is bad, false otherwise.
+     */
+    private boolean isBadInput(String[] input) {
+        // scan all tokens in the line
+        for (int i = 0; i < input.length; i++) {
+            // if token is a NUMBER
+            try {
+                int token = Integer.parseInt(input[i]);
+                if (token < 0 || token > this.model.getBoardSize()) {
+                    return true;
+                }
+            }
+            // if token is a STRING
+            catch (NumberFormatException e) {
+                String token = input[i];
+                if (!token.equals("q")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
 }
--- Module 09/Assignment06/test/TestMarbleSolitaireControllerImpl.java
+++ Module 09/Assignment06/test/TestMarbleSolitaireControllerImpl.java
@@ -7,6 +7,9 @@ import cs5004.marblesolitaire.view.MarbleSolitaireView;
 import org.junit.Test;
 import org.junit.Before;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.StringReader;
 
 import static org.junit.Assert.assertEquals;
@@ -22,6 +25,23 @@ public class TestMarbleSolitaireControllerImpl {
     public void setUp() {
         model = new EnglishSolitaireModel();
         view = new MarbleSolitaireTextView(model, System.out);
-        controller = new MarbleSolitaireControllerImpl(model, view, new StringReader());
+        try {
+            FileReader fileReader = new FileReader("moves.txt");
+            controller = new MarbleSolitaireControllerImpl(model, view, fileReader);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
+    @Test
+    public void testPlayGame() {
+        try {
+            controller.playGame();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
