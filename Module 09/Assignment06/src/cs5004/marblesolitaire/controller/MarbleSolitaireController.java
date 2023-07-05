7d3c762eee9fad54457fa772ac1be5b76f55caca
cyberShah <shah.pranc@northeastern.edu>
2023-07-05 08:54:30 +0530

    tests

--- Module 09/Assignment06/src/cs5004/marblesolitaire/controller/MarbleSolitaireController.java
+++ Module 09/Assignment06/src/cs5004/marblesolitaire/controller/MarbleSolitaireController.java
@@ -1,8 +1,11 @@
 package cs5004.marblesolitaire.controller;
 
 public interface MarbleSolitaireController {
+
     /**
-     * Play a new game of Marble Solitaire using the provided model.
+     * Play a new game of Marble Solitaire using the model.
+     * Plays the game until it is over or the user quits.
+     * Uses Readable to get input from the user.
      *
      * @throws IllegalStateException if it encounters issues with input or output.
      */
