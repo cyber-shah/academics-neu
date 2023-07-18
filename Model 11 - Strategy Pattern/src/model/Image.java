b9f0ba075646cae345fdd8909069a1dc982ddf9b
cyber-shah <shah.pranc@northeastern.edu>
2023-07-18 12:00:12 +0530

    created Image

---
+++ Model 11 - Strategy Pattern/src/model/Image.java
@@ -0,0 +1,60 @@
+package model;
+
+public class Image {
+  private final int width;
+  private final int height;
+  private Pixel[] pixelsList;
+
+  /**
+   * Constructor for Image.
+   *
+   * @param width  int value of width.
+   * @param height int value of height.
+   */
+  public Image(int width, int height) {
+    this.width = width;
+    this.height = height;
+    int i = width * height;
+    this.pixelsList = new Pixel[i];
+  }
+
+  /**
+   * Getter for width.
+   *
+   * @return int value of width.
+   */
+  public int getWidth() {
+    return this.width;
+  }
+
+  /**
+   * Getter for height.
+   *
+   * @return int value of height.
+   */
+  public int getHeight() {
+    return this.height;
+  }
+
+  /**
+   * Getter for pixelsList.
+   *
+   * @return Pixel[] value of pixelsList.
+   */
+  public Pixel[] getPixelsList() {
+    return this.pixelsList;
+  }
+
+
+  public void setPixelsList(Pixel[] pixelsList) {
+    this.pixelsList = pixelsList;
+  }
+
+  public void setPixel(int i, Pixel pixel) {
+    this.pixelsList[i] = pixel;
+  }
+
+  public Pixel getPixel(int i) {
+    return this.pixelsList[i];
+  }
+}
