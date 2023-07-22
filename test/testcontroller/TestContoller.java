ec0acd2c8be5b82a2e3d5bf9675fb3d45883d9c1
cyber-shah <shah.pranc@northeastern.edu>
2023-07-22 20:12:25 +0530

    testing done

--- test/testcontroller/TestContoller.java
+++ test/testcontroller/TestContoller.java
@@ -49,7 +49,6 @@ public class TestContoller {
    * 7.
    *
    */
-
   @Test
   public void testLoadCommand() {
     mockView = new MockViewImplementation();
@@ -60,5 +59,77 @@ public class TestContoller {
     ControllerInterface controller = new ControllerImplementation(mockImageDatabase, mockView, inReadable, mockCommandsManager);
     controller.runProgram();
     assertEquals("Args 0: LOAD Args 1: abc.ppm Args 2: abc ", mockCommandsManager.getLog());
+  }
+
+  @Test
+  public void testSaveCommand() {
+    mockView = new MockViewImplementation();
+    MockImageDatabase mockImageDatabase = new MockImageDatabase();
+    // 1. put the commands in a string
+    Readable inReadable = new StringReader("SAVE abc.ppm abc\n");
+    // 2. pass the string to the controller
+    ControllerInterface controller = new ControllerImplementation(mockImageDatabase, mockView, inReadable, mockCommandsManager);
+    controller.runProgram();
+    assertEquals("Args 0: SAVE Args 1: abc.ppm Args 2: abc ", mockCommandsManager.getLog());
+  }
+
+  @Test
+  public void testBrightenCommand() {
+    mockView = new MockViewImplementation();
+    MockImageDatabase mockImageDatabase = new MockImageDatabase();
+    // 1. put the commands in a string
+    Readable inReadable = new StringReader("BRIGHTEN abc.ppm abc\n");
+    // 2. pass the string to the controller
+    ControllerInterface controller = new ControllerImplementation(mockImageDatabase, mockView, inReadable, mockCommandsManager);
+    controller.runProgram();
+    assertEquals("Args 0: BRIGHTEN Args 1: abc.ppm Args 2: abc ", mockCommandsManager.getLog());
+  }
+
+  @Test
+  public void testLumaCommand() {
+    mockView = new MockViewImplementation();
+    MockImageDatabase mockImageDatabase = new MockImageDatabase();
+    // 1. put the commands in a string
+    Readable inReadable = new StringReader("LUMA abc.ppm abc\n");
+    // 2. pass the string to the controller
+    ControllerInterface controller = new ControllerImplementation(mockImageDatabase, mockView, inReadable, mockCommandsManager);
+    controller.runProgram();
+    assertEquals("Args 0: LUMA Args 1: abc.ppm Args 2: abc ", mockCommandsManager.getLog());
+  }
+
+  @Test
+  public void testIntensityCommand() {
+    mockView = new MockViewImplementation();
+    MockImageDatabase mockImageDatabase = new MockImageDatabase();
+    // 1. put the commands in a string
+    Readable inReadable = new StringReader("INTENSITY abc.ppm abc\n");
+    // 2. pass the string to the controller
+    ControllerInterface controller = new ControllerImplementation(mockImageDatabase, mockView, inReadable, mockCommandsManager);
+    controller.runProgram();
+    assertEquals("Args 0: INTENSITY Args 1: abc.ppm Args 2: abc ", mockCommandsManager.getLog());
+  }
+
+  @Test
+  public void testValueCommand() {
+    mockView = new MockViewImplementation();
+    MockImageDatabase mockImageDatabase = new MockImageDatabase();
+    // 1. put the commands in a string
+    Readable inReadable = new StringReader("VALUE abc.ppm abc\n");
+    // 2. pass the string to the controller
+    ControllerInterface controller = new ControllerImplementation(mockImageDatabase, mockView, inReadable, mockCommandsManager);
+    controller.runProgram();
+    assertEquals("Args 0: VALUE Args 1: abc.ppm Args 2: abc ", mockCommandsManager.getLog());
+  }
+
+  @Test
+  public void testComponentCommand() {
+    mockView = new MockViewImplementation();
+    MockImageDatabase mockImageDatabase = new MockImageDatabase();
+    // 1. put the commands in a string
+    Readable inReadable = new StringReader("COMPONENT red abc.ppm abc\n");
+    // 2. pass the string to the controller
+    ControllerInterface controller = new ControllerImplementation(mockImageDatabase, mockView, inReadable, mockCommandsManager);
+    controller.runProgram();
+    assertEquals("Args 0: COMPONENT Args 1: red Args 2: abc.ppm Args 3: abc ", mockCommandsManager.getLog());
   }
 }
