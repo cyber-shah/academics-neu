c1dd14bbb63655e5cf694a063fb54438e32f5956
cyber-shah <pranchx@gmail.com>
2023-10-20 00:14:42 -0400

    shhs

--- change_dates.sh
+++ change_dates.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-git filter-branch -f --env-filter '
+git filter-branch --env-filter '
 
-n=$GIT_COMMIT
-i=0
+OLD_GIT_AUTHOR_DATE=$(git show -s --format="%ai" $GIT_COMMIT)
+OLD_GIT_COMMITTER_DATE=$(git show -s --format="%ci" $GIT_COMMIT)
 
-while [ $i -lt 22 ]
-do
-    if [ $n = $GIT_COMMIT ]
-    then
-        export GIT_AUTHOR_DATE="2023-08-23T00:00:00"
-        export GIT_COMMITTER_DATE="2023-08-23T00:00:00"
-    fi
-    n=$(git rev-parse $n^)
-    i=$((i+1))
-done
+if [[ "$OLD_GIT_AUTHOR_DATE" == "2023-10-03"* ]] && [[ "$OLD_GIT_COMMITTER_DATE" == "2023-10-03"* ]]
+then
+    export GIT_AUTHOR_DATE=$(date -d"$OLD_GIT_AUTHOR_DATE -35 days" +"%Y-%m-%dT%H:%M:%S")
+    export GIT_COMMITTER_DATE=$(date -d"$OLD_GIT_COMMITTER_DATE -35 days" +"%Y-%m-%dT%H:%M:%S")
+fi
 
-' -- --all
-
+' HEAD
