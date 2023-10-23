a5685949334de338d017a112e424ee08abbcae23
cyber-shah <pranchx@gmail.com>
2023-10-22 21:19:30 -0400

    impoprting from csv

--- CS 5200 - DBMS/HW6/Hwk6crimequeriesShahP.sql
+++ CS 5200 - DBMS/HW6/Hwk6crimequeriesShahP.sql
@@ -1,6 +1,6 @@
-drop database if exists crime_db2022ShahP;
-create database crime_db2022ShahP;
-use crime_db2022ShahP;
+drop database if exists crime_db2022shahp;
+create database crime_db2022shahp;
+use crime_db2022shahp;
 
 
 create table district (
@@ -20,38 +20,46 @@ create table neighbourhood (
     foreign key(district_code) references district(district_code)
 );
 
-
 create table offense_code (
 	o_code				varchar(5)			not null,
-    description			varchar(50)			not null
+    description			varchar(50)			not null,
+    
+    primary key(o_code)
 );
 
+drop table street_locations;
+-- create table street_locations (
+-- 	street				varchar(20)			not null,
+--     latitude 			decimal(10,8) 		not null,
+-- 	longitude 			decimal(11,8) 		not null,
+--     
+--     primary key(latitude, longitude, street)
+-- );
+-- create index latitude on street_locations(latitude);
+-- create index longitude on street_locations(longitude);
 
+
+drop table incidents;
 create table incidents (
-	incident_id			int			auto_increment,
+	incident_id			varchar(20)			not null,
     o_code				varchar(5)			not null,
     shooting			boolean				not null		default(false),
     
     district_code		varchar(5)			not null,
     reporting_area		varchar(10),
-    street				varchar(20),
-    latitude 			decimal(10,8),
-	longitude 			decimal(11,8),
-    
+    latitude 			decimal(10,8) 		not null,
+	longitude 			decimal(11,8) 		not null,
+    street			 	varchar(20)			not null,
+     
     occurred_date		datetime,
     
     primary key(incident_id),
-    foreign key(district_code) references district(district_code)
+    foreign key(district_code) references district(district_code),
+    foreign key (o_code) references offense_code(o_code)
 );
 
-create index code_idx on offense_code(o_code);
-
-alter table incidents 
-add constraint fk_o_code
-foreign key (o_code) references offense_code(o_code);
-
-
-
-
-
-
+select * from district; 
+select * from neighbourhood; 
+select * from offense_code;
+select * from street_locations;
+select * from incidents;
