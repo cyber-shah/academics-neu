c8bdf67dbae341b88096bb018bc72776822ccb19
cyber-shah <pranchx@gmail.com>
2023-10-22 21:29:11 -0400

    done importing

--- CS 5200 - DBMS/HW6/Hwk6crimequeriesShahP.sql
+++ CS 5200 - DBMS/HW6/Hwk6crimequeriesShahP.sql
@@ -2,54 +2,49 @@ drop database if exists crime_db2022shahp;
 create database crime_db2022shahp;
 use crime_db2022shahp;
 
-
+drop table district;
 create table district (
-	district_code 		varchar(5)		not null,
-    district_name		varchar(20)		not null,
+	district_code 		varchar(10),
+    district_name		varchar(50),
     
     primary key(district_code),
     unique(district_code, district_name)
 );
+select * from district; 
 
 
+drop table neighbourhood;
 create table neighbourhood (
-	n_name					varchar(20)		not null,
-    district_code			varchar(5)		not null,
+	n_name					varchar(50)		not null,
+    district_code			varchar(10)		not null,
     
     primary key(district_code, n_name),
     foreign key(district_code) references district(district_code)
 );
+select * from neighbourhood; 
 
+
+drop table offense_code;
 create table offense_code (
-	o_code				varchar(5)			not null,
-    description			varchar(50)			not null,
+	o_code				int						not null,
+    description			varchar(200)			not null,
     
     primary key(o_code)
 );
-
-drop table street_locations;
--- create table street_locations (
--- 	street				varchar(20)			not null,
---     latitude 			decimal(10,8) 		not null,
--- 	longitude 			decimal(11,8) 		not null,
---     
---     primary key(latitude, longitude, street)
--- );
--- create index latitude on street_locations(latitude);
--- create index longitude on street_locations(longitude);
+select * from offense_code;
 
 
 drop table incidents;
 create table incidents (
-	incident_id			varchar(20)			not null,
-    o_code				varchar(5)			not null,
+	incident_id			varchar(30)			not null,
+    o_code				int					not null,
     shooting			boolean				not null		default(false),
     
-    district_code		varchar(5)			not null,
-    reporting_area		varchar(10),
+    district_code		varchar(10)			not null,
+    reporting_area		varchar(20),
     latitude 			decimal(10,8) 		not null,
 	longitude 			decimal(11,8) 		not null,
-    street			 	varchar(20)			not null,
+    street			 	varchar(100)		not null,
      
     occurred_date		datetime,
     
@@ -57,9 +52,4 @@ create table incidents (
     foreign key(district_code) references district(district_code),
     foreign key (o_code) references offense_code(o_code)
 );
-
-select * from district; 
-select * from neighbourhood; 
-select * from offense_code;
-select * from street_locations;
 select * from incidents;
