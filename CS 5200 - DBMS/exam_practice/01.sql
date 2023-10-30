ddfe7be4bae89c0f1e1c1c06daa7314e9432affe
cyber-shah <pranchx@gmail.com>
2023-10-29 21:57:28 -0400

    prepping for class

--- CS 5200 - DBMS/exam_practice/01.sql
+++ CS 5200 - DBMS/exam_practice/01.sql
@@ -13,5 +13,24 @@ inner join vendors on vendor_contacts.vendor_id = vendors.vendor_id;
 
 -- Retrieve the invoice numbers and their corresponding invoice total for invoices 
 -- with a total greater than $1,000.
+select 	i.invoice_id,
+		i.invoice_total,
+        i.payment_total,
+        i.credit_total
+        
+from invoices as i
+
+where (i.invoice_total > 1000 or i.payment_total > 1000 or i.credit_total > 1000);
+
+
+-- Find the vendor names and the total number of invoices issued by each vendor.
+select 	vendor_name,
+		count(i.invoice_id)
+        
+from vendors
+
+left join invoices i on vendors.vendor_id = i.vendor_id
+group by(vendor_name)
+order by(count(i.invoice_id)) desc;
 
 
