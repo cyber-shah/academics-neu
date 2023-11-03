240cf344d5ff8a9d1ed66f73914c026218712e9c
cyber-shah <pranchx@gmail.com>
2023-11-02 23:08:17 -0400

    1 - n2 time

--- leecode/HR-greedy-cloudy.js
+++ leecode/HR-greedy-cloudy.js
@@ -37,7 +37,7 @@
  *  4. LONG_INTEGER_ARRAY r
  */
 
-function maximumPeople(p, x, y, r) {
+function maximumPeople1(p, x, y, r) {
     // p = population of the town(p1, p2, ... , pi)
     // x = location of the town(x1, x2, ....., xi)
     // y = location of clouds(y1, y2, ......, yi)
@@ -67,6 +67,67 @@ function maximumPeople(p, x, y, r) {
     }
 
     return max_score;
+}
+
+function maximumPeople(p, x, y, r) {
+    // p = population of the town(p1, p2, ... , pi)
+    // x = location of the town(x1, x2, ....., xi)
+    // y = location of clouds(y1, y2, ......, yi)
+    // r = range of clouds r_i is the range of cloud y_i
+    let events = [];
+    
+    // put the towns first
+    for (var i = 0; i < x.length; i++) {
+        events.push({type : 'town', pos : x[i], index : i});
+    }
+    
+    // then the clouds
+    for (var i = 0; i < y.length; i++) {
+        events.push({type : 'start', pos : y[i] - r[i], cloud : i});
+        events.push({type : 'end', pos : y[i] + r[i], cloud : i});
+    }
+    
+    // get the events first
+    events.sort((a, b) => a.pos - b.pos || (a.type === 'town') - (b.type === 'town'));
+    
+    console.log(events);
+    
+    let cloud_count = 0;
+    var active_cloud = 0;
+    var cloud_score = new Array(y.length).fill(0);
+    var sunny_population = 0;
+    
+    // for all events
+    for (var i = 0; i < events.length; i++) {
+        // if its a town
+        if (events[i].type === 'town') {
+            if (cloud_count === 1) {
+                console.log("cloudcount is 1")
+                cloud_score[active_cloud] += p[events[i].index];
+            }
+            else if (cloud_count === 0) {
+                sunny_population += p[events[i].index];
+                console.log("added sunny pop " + sunny_population);
+            }
+        }
+        // if starts
+        else if (events[i].type === 'start') {
+            cloud_count++;
+            if (cloud_count === 1) {
+                active_cloud = events[i].cloud;
+            }
+        }
+        // if ends
+        else if (events[i].type === 'end') {
+            cloud_count--;
+            if (cloud_count === 0) {
+                active_cloud = -1;
+            }
+        }
+    }
+
+    console.log(cloud_score);
+    return sunny_population + Math.max(...cloud_score);
 }
 
 function main() {
