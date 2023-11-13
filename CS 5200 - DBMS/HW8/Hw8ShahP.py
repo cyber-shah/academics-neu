cec0a81c8a66f6704e3b2e418d4134b988cd3657
cyber-shah <pranchx@gmail.com>
2023-11-12 19:48:48 -0500

    asks for uid and password

--- CS 5200 - DBMS/HW8/Hw8ShahP.py
+++ CS 5200 - DBMS/HW8/Hw8ShahP.py
@@ -1,28 +1,60 @@
 import mysql.connector
 
-# Replace these values with your MySQL server information
-host = "localhost"
-user = "root"
-password = "aH24fkWu*}Q"
-database = "musicshahp"
 
-# Establish a connection to the MySQL server
-connection = mysql.connector.connect(
-    host=host,
-    user=user,
-    password=password,
-    database=database
-)
+class DatabaseManager:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = None
+        self.cursor = None
 
-curor = connection.cursor()
+    def connect(self):
+        self.connection = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        self.cursor = self.connection.cursor()
 
-query = "SELECT * FROM albums"
+    def execute_query(self, query):
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
 
-curor.execute(query)
+    def close_connection(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
 
-results = curor.fetchall()
-for row in results:
-	print(row)
 
-cursor.close()
-connection.close()
+def main():
+    # Replace these values with your MySQL server information
+    host = "localhost"
+    user = input("Enter MySQL username: ")
+    password = input("Enter MySQL password for " + user + " : ")
+    database = "musicshahp"
+
+    # Create an instance of the DatabaseManager
+    db_manager = DatabaseManager(host, user, password, database)
+
+    try:
+        # Establish a connection to the MySQL server
+        db_manager.connect()
+
+        # Execute a query
+        query = "SELECT * FROM albums"
+        results = db_manager.execute_query(query)
+
+        # Print the results
+        for row in results:
+            print(row)
+
+    finally:
+        # Close the connection
+        db_manager.close_connection()
+
+if __name__ == "__main__":
+    main()
