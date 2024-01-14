9bdd5a2ab12284ed21bed5ec86fd3092444a08c7
cyber-shah <shah.pranc@northeastern.edu>
2024-01-13 21:35:16 -0500

    json messages

--- 6740-networks/HW1/chat_client.py
+++ 6740-networks/HW1/chat_client.py
@@ -1,6 +1,7 @@
 import socket
 import argparse
 import threading
+import json
 
 
 class Client:
@@ -9,15 +10,36 @@ class Client:
         self.server_ip = server_ip
         self.server_port = server_port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.sendto(self.username.encode(),
+        # the first message to the server should always be the username
+        # so that the server can register the client
+        self.register()
+
+    def register(self):
+        message = {"command": "register", "username": self.username}
+        self.sock.sendto(json.dumps(message).encode(),
                          (self.server_ip, self.server_port))
-        print(
-            f"Connected to {self.server_ip}:{self.server_port} as {self.username}")
+
+        # wait for the server to respond for confirmation
+        data, server_address = self.sock.recvfrom(1024)
+        data = json.loads(data.decode())
+        if data['response'] == "success":
+            print(data['message'])
+        else:
+            print(data['message'])
+            exit()
+
+    def listen(self):
+        while True:
+            data, server_address = self.sock.recvfrom(1024)
+            data = json.loads(data.decode())
+            if data['response'] == "success":
+                print(data['message'])
 
     def parse_input(self, input):
         message = input.capitalize().split()
         if message[0] == "List":
-            print("List of users:")
+            self.sock.sendto(json.dumps({"command": "list"}).encode(
+            ), (self.server_ip, self.server_port))
         elif message[0] == "Send":
             print("Sending message")
         elif message[0] == "Exit":
@@ -30,15 +52,16 @@ class Client:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Client for chat application, starts with -u <username> -sip <server-ip> -sp <PORT>")
+        description="Client for chat application, \
+        starts with -u <username> -sip <server-ip> -sp <PORT>")
     parser.add_argument("-u", help="username", required=True)
     parser.add_argument("-sip", help="server ip", required=True)
     parser.add_argument("-sp", help="port number", required=True)
     args = parser.parse_args()
     client = Client(args.u, args.sip, int(args.sp))
     # start a thread to listen to messages from the server
-    threading.Thread(target=client.start).start()
 
     while True:
         message = input()
         client.parse_input(message)
+        client.listen()
--- 6740-networks/HW1/chat_server.py
+++ 6740-networks/HW1/chat_server.py
@@ -1,5 +1,6 @@
 import socket
 import argparse
+import json
 
 HOST = "localhost"
 
@@ -7,15 +8,19 @@ HOST = "localhost"
 Server's job:
 1. LIST : show all the clients connected
 2. REGISTER : register the client with this format
-    -u <username> -sip <server-ip> -sp <PORT> 
+    -u <username> -sip <server-ip> -sp <PORT>
 3. SEND <username> <message> : send message to the user
-    emit the message to the client : <FROM IP:PORT Carole> : <Message> 
+    emit the message to the client : <FROM IP:PORT Carole> : <Message>
 4. START : start the server
     python server.py -sp <PORT>
 """
 
 
 class Server:
+    """
+    Server class to handle all the server side operations
+    All messages MUST be sent in JSON format
+    """
     clients_details = {}
 
     def __init__(self, port):
@@ -25,21 +30,70 @@ class Server:
         print("Server Initialized...")
 
     def start(self):
+        """
+        Start the server and
+        listen to incoming connections
+        """
         while True:
             data, client_address = self.sock.recvfrom(1024)
-            host, port = client_address
-            username = data.decode()
-            if username in self.clients_details:
-                self.sock.sendto(
-                    f"Username {username} already exists, please try again".encode(), client_address)
-                continue
+            # see what the data type is and
+            # then forward it to the appropriate function
+            data = json.loads(data.decode())
+            if data['command'] == 'register':
+                message = self.register_client(client_address, data)
+            elif data['command'] == 'list':
+                message = self.list_clients()
+            elif data['command'] == 'send':
+                message = self.send_client_address(data)
             else:
-                self.sock.sendto(
-                    f"Username {username} is registered".encode(), client_address)
-            self.clients_details[username] = {'IP': host, 'PORT': port}
-            print(
-                f"New connection from {self.clients_details[username]} at {username}")
-            print(self.clients_details)
+                message = {'response': 'error',
+                           'message': 'Invalid command'}
+            # eventually send the message to the client
+            self.sock.sendto(json.dumps(message).encode(), client_address)
+
+    def register_client(self, client_address, data):
+        """
+        Checks if the client is already registered
+        and registers the client if not
+
+        :return a message
+        """
+        # 1. see if the client is already registered
+        username = data['username']
+        if username in self.clients_details:
+            message = {'response': 'error',
+                       'message': 'Username already exists'}
+            return message
+        else:
+            message = {'response': 'success',
+                       'message': 'Welcome to the chat app'}
+            self.clients_details[username] = {
+                'IP': client_address[0], 'PORT': client_address[1]}
+            print(f"New connection from \
+                {self.clients_details[username]} at {username}")
+            return message
+
+    def list_clients(self):
+        """
+        List all the clients connected
+
+        :return a message
+        """
+        message = {'response': 'success',
+                   'message': 'List of clients\n'}
+        for client in self.clients_details:
+            message['message'] += f"    {client}\n"
+        return message
+
+    def send_client_address(self, username):
+        """
+        Send the client address to the client
+        """
+        if username not in self.clients_details:
+            return {'response': 'error',
+                    'message': 'Username does not exist'}
+        else:
+            return self.clients_details[username]
 
 
 if __name__ == "__main__":
