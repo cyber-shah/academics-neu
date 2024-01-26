55c1f1e45de8c9a5034c737be3a3a72851e303c8
cyber-shah <pranchx@gmail.com>
2024-01-25 21:13:03 -0500

    completed hw

--- 5700-networks/hw-3/client.py
+++ 5700-networks/hw-3/client.py
@@ -1,14 +1,24 @@
-#Client program to send data to a server via a socket, and then have that same data echoed back.
+"""
+Pranchal Shah
+CS 5700 - Computer Networks
+HW 3 - Sockets Programming
+"""
+
 import socket
+import sys
 
-HOST = "127.0.0.1" #The server's host name or IP address
-PORT = 65432 #The port used by the server
+HOST = "127.0.0.1"  # The server's host name or IP address
+PORT = 65438  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #Create a new socket using teh given address famiily, socket
-    #type and protocol number
-    s.connect((HOST, PORT)) #Connect to a remote socket at the address and port
-    s.sendall(b"Hello networking!") #Send data to the socket. The socket must be connected to a remote socket. Data type
-    #is byte
-    data = s.recv(1024) #Receive data from the socket. The return type is a bytes object representing the data received.
-    #The maximum amount of data to be received in each chunk is 1024 as specified.
-    print(f"Received {data!r}") #print to console
+number = sys.argv[1]
+
+# TODO: check if we need encoding
+# create TCP socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.send(number.encode('utf-8'))
+    data = s.recv(1024)
+    # The maximum amount of data to be received in each chunk
+    # is 1024 as specified.
+    print(f"Sent {number} and received {data!r}")  # print to console
+
