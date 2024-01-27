e017f6518d09fcabf914d459d5d2cc614b0a2dc6
cyber-shah <shah.pranc@northeastern.edu>
2024-01-27 14:53:40 -0500

    flow setup

--- 6740-security/ps-2/fcrypt.py
+++ 6740-security/ps-2/fcrypt.py
@@ -1,4 +1,21 @@
 """
+Here's the flow of the program:
+1. get the senders SK and dest PK along with plaintext
+2. generate a symmetric key using KSD (key derivation function) and encrypt the key with RSA
+3. use the symmetric key to encrypt the plaintext
+4. generate a signature using the senders SK and the plaintext
+5. output the ciphertext and signature to a file
+
+
+6. get the desk SK and senders PK along with the ciphertext
+7. decrypt the symmetric key using RSA
+8. use the symmetric key to decrypt the ciphertext
+9. verify the signature using the senders PK and the plaintext
+10. output the plaintext to a file
+"""
+
+
+"""
 For encryption and signatures generating ciphertext_file: ALL MUST BE FILES
 python fcrypt.py -e <dest_PK> <sender_SK> <in_plaintext> <out_ciphertext>
 
@@ -15,10 +32,9 @@ Three modules:
 2. test the program
 """
 
+
 import argparse
 import cryptography.hazmat.primitives
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Encrypt and sign files")
     parser.add_argument("-e", "--encrypt", action="store_true")
